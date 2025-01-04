86b2730d3ac9bf8f80f66ce5188c96c06db2d81a
Johannes Alexander Putra, B.Ed. <71767003+Itsjohanes@users.noreply.github.com>
2025-01-04 10:23:09 +0700

    Update layout.tsx

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -79,9 +79,9 @@ export default async function RootLayout({
                 />
               ) : (
                 <div className="flex flex-col items-center py-28 lg:flex-row">
-                  <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
-                    By Johannes Alexander Putra
-                  </h3>
+                  <h6 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
+                    Copyright 2025 Johannes Alexander Putra
+                  </h6>
                   <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
                    
                   </div>
